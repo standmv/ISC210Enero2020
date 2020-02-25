fdf4b236b468ff9720a4227fbfe56d5ac6070453
Standmv <standmv13@gmail.com>
2020-02-25 18:11:55 -0400

    cambios

---
+++ UltimateRPG/Assets/Scripts/UltimateEssence/ImageScrollBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageScrollBehaviour : MonoBehaviour
+{
+    float _currentPositionX, _scrollingSpeedX = 0.3f;
+    MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentPositionX += _scrollingSpeedX * Time.deltaTime;
+        meshRenderer.material.mainTextureOffset = new Vector2(_currentPositionX, 0);
+    }
+}
