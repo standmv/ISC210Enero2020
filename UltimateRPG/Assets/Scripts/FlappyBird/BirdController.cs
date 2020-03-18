2a6a6afe3047daaeed7d2eaa9a5806323cd5b89b
Standmv <standmv13@gmail.com>
2020-03-17 20:35:33 -0400

    Examen y demas

---
+++ UltimateRPG/Assets/Scripts/FlappyBird/BirdController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    
+    Vector3 _clickForce;
+    Rigidbody BirdRefence;
+    bool _gameStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _clickForce = new Vector3(0, 200, 0);
+        BirdRefence = gameObject.GetComponent<Rigidbody>();
+        BirdRefence.velocity = Vector3.zero;
+        //gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    private void FixedUpdate()
+    {
+        if (_gameStarted)
+        {
+            gameObject.transform.Translate(BirdRefence.velocity * Time.deltaTime + Physics.gravity * Mathf.Pow(Time.deltaTime, 2) / 2);
+            BirdRefence.velocity += Physics.gravity * Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _gameStarted = true;
+            BirdRefence.velocity = Vector3.zero;
+            BirdRefence.AddForce(_clickForce);
+        }
+    }
+}
---
+++ UltimateRPG/Assets/Scripts/FlappyBird/MapController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject pipe;
+    public GameObject pipeDown;
+    public GameObject goal;
+    void Start()
+    {
+        CargarMapa();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CargarMapa()
+    {
+        GameObject nuevaCelda = null;
+        //Primero, lee el archivo de texto del mapa.
+        var contenido = Resources.Load<TextAsset>("Map");
+        //Segundo, recorre el contenido, y para cada celda:
+        int i, j;
+        j = 0;
+        foreach (string LineaActual in contenido.text.Split('\n'))
+        {
+            i = 0;
+            foreach (char celdaActual in LineaActual)
+            {
+                switch (celdaActual)
+                {
+                    case '+':
+                        nuevaCelda = pipe;
+                        break;
+                    case ':':
+                        nuevaCelda = goal;
+                        break;
+                    case '*':
+                        nuevaCelda = pipeDown;
+                        break;
+                    default:
+                        i++;
+                        continue;
+                }
+                nuevaCelda = Instantiate(nuevaCelda, new Vector3(i - 6f, -j + 4f, 0), Quaternion.identity);
+                i++;
+            }
+            j++;
+        }
+        //Instacia los objecto
+    }
+}
