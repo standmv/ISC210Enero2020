bef427f28483c3deffaa5224cd5a984252c37ca9
Standmv <standmv13@gmail.com>
2020-04-02 03:31:32 -0400

    Menu Casi Listo

---
+++ UltimateRPG/Assets/Scripts/Menu/GameOptionsLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization;
+
+public class GameOptionsLoader : MonoBehaviour
+{
+    string _localPath = "ultimateGameOptions.xml";
+
+    private void Start()
+    {
+        Debug.Log("En el player prefs:" + PlayerPrefs.GetString("PlayerName", "Jhon Doe"));
+        LoadGameOptions();
+    }
+    public void SaveGameOptions()
+    {
+        using (Stream fileStream = new FileStream(Application.persistentDataPath + "\\" + _localPath, FileMode.Create))
+        {
+            DataContractSerializer dataContract = new DataContractSerializer(typeof(GameOptions));
+            dataContract.WriteObject(fileStream, GameOptions.Instance);
+        }
+    }
+
+    public void LoadGameOptions()
+    {
+        using (Stream fileStream = new FileStream(Application.persistentDataPath + "\\" + _localPath, FileMode.Open))
+        {
+            DataContractSerializer dataContract = new DataContractSerializer(typeof(GameOptions));
+            GameOptions.Instance = (GameOptions)dataContract.ReadObject(fileStream);
+
+        }
+    }
+}
